8b7b36420eba1ee2c8086ee94b8bdd21520c25fa
Hoang Nguyen <hoang.nguyen1992@hotmail.com>
2014-10-22 12:43:29 +0000

    delete folder js-binding

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,3 +1,17 @@
 /**
  * Created by hoangnguyen on 10/22/14.
  */
+
+var GameLayer = cc.Layer.extend({
+    ctor : function(){
+        this._super();
+        this.init();
+    },
+    init : function(){
+        initBackground();
+    },
+    initBackground: function(){
+        
+    }
+    
+});
